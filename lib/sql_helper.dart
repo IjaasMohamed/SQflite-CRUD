3086ae39201621200eb21de21aad6edc01bb0701
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-12-11 14:54:51 +0530

    initial

---
+++ lib/sql_helper.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLHelper{
+  static Future<void> createTable(sql.Database database) async {
+    await database.execute("""CREATE TABLE item(
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      title TEXT,
+      description TEXT,
+      createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )
+    """);
+  }
+  static Future <sql.Database> db() async {
+    return sql.openDatabase(
+      'dbestech.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTable(database);
+      },
+    );
+  }
+  static Future<int> createItem(String title, String? description) async {
+    final db = await SQLHelper.db();
+
+    final data = {'title':title, 'description':description};
+    final id = await db.insert('items', data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+  static Future <List<Map<String, dynamic>>> getItems() async {
+    final db = await SQLHelper.db();
+    return db.query('items', orderBy: "id");
+  }
+}
